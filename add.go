cbf17017396a2b4ecd40ad7be453728bca065d8f
sashbyte <sashenmoodley321@gmail.com>
2024-05-07 18:37:36 +0200

    inital commit

---
+++ add.go
@@ -0,0 +1,6 @@
+// Package go_module_test test package with misc functions
+package go_module_test
+
+func Add(num1 int, num2 int) int {
+	return num1 + num2
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module go_module_test
+
+go 1.22
