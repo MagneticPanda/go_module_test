494a2c6eaf0469f19424a4ca3627063ee6251b9a
sashbyte <sashenmoodley321@gmail.com>
2024-05-09 12:40:22 +0200

    update package name

--- add.go
+++ add.go
@@ -1,5 +1,5 @@
 // Package go_module_test is a single-package testing module for importing third-party dependencies into a module
-package go_module_test
+package test_package
 
 // Add adds 2 integer values together and returns the result
 func Add(num1 int, num2 int) int {
